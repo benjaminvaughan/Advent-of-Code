084459aad2f326b1535c3b53d0f01ef582317155
benjaminvaughan <vaughanb18@gmail.com>
2019-12-12 13:56:12 -0500

    ok actually did day 1 properly

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
 from math import *
+import numpy as np
 
 f_obj = open('day1.txt')
 
@@ -7,6 +8,26 @@ for line in f_obj:
     m = float(line)
     fuel = floor ( m / 3.) - 2
     fuel_vals.append(fuel)
+
+ans = np.sum(fuel_vals)
+print(ans, 'answer to part 1')
+
+def HUH(lister):
+    print(lister, 'lister')
+    out_lister = []
+    print(len(lister))
+    for i in range(len(lister)):
+        fuel = floor ( lister[i] / 3.) - 2
+        if fuel > 0:
+            out_lister.append(fuel)
+    return out_lister
+
+total = ans
+while len(fuel_vals) != 0:
+    fuel_vals = HUH(fuel_vals)
+    total += np.sum(fuel_vals)
+
+print(total, 'answer to part 2')
 
 f_obj.close()
 
